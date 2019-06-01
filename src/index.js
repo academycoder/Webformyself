d007668bcdb3448a77d2496394e9803f9651a8e2
fillika <fillika@users.noreply.github.com>
2019-06-01 21:27:43 +0300

    Посмотрел видео 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log ('Hello world');
+import AppService from './modules/app.service'
+import {config} from './modules/config'
+import './modules/header.component'
+
+console.log('Config key: ', config.key)
+
+const service = new AppService('Hello WOrld!')
+service.log()
