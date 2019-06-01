fad687536f2c6c29a5d0ef98f295117d25e4636e
fillika <fillika@users.noreply.github.com>
2019-06-02 01:42:41 +0300

    Урок 6
    Создали стили в отдельном файле. Добавили файл для сжатия CSS и JS(из-за разницы в синтаксисе между ES5 и ES6 пока строчку Uglify Я закоментировал)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path'); //Втроеный модуль nodejs, который мы получаем с помощью конструкции require. Данный модуль позволяет работать с путями, упрощает работу
-const HTMLPLugin = require('html-webpack-plugin')
+const HTMLPLugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // Для сжатия CSS стилей
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 
 module.exports = {
@@ -8,17 +11,26 @@ module.exports = {
     filename: 'bundle.js', // Название того файла, который должен получится
     path: path.resolve(__dirname, 'dist')// Путь, куда мы должны положить все исходные статические файлы, __dirname - это системная переменная, которая указывает ткущую папку, а второй параметр (дист) - куда нужно все это сложить.
   },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({}), // Для сжатия CSS стилей
+      // new UglifyJsPlugin({}) // для сжатия JS
+    ]
+  },
   plugins:[ 
     new HTMLPLugin({ // Плагин, позволяет компилировать файлы HTML с автоматическим добавлением ссылок на js файлы или css
       filename: 'index.html', // Название файла, которое мы получим на выходе в папку dist
       template: './src/index.html' //Указываем шаблон, который берем за основу
     }),
+    new MiniCssExtractPlugin({ // Берет мой CSS файл и компилирует его в отдельный файл style css
+      filename: 'style.css'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/, // Указываем расширение файла, на который будет влиять loader
-        use: ['style-loader','css-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
+        use: [MiniCssExtractPlugin.loader,'css-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
       }
     ]
   }
