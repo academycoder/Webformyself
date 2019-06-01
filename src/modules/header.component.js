56353d322207108daa2c2637853191f307835c0b
fillika <fillika@users.noreply.github.com>
2019-06-01 23:06:21 +0300

    Видео 4
    Подключили библиотеку, посмотрели, как все работает. Так же увидел, как можно подключить --watch и как он работает

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,1 +1,9 @@
-console.log('Header Component')
+import $ from 'jquery' // Мы не указали полного пути, т.к. вебпак сам понимает, что нужно идти в папку node modules и искать там жуйквери
+
+$('<h1 />')
+  .text('Hello world from JQuery')
+  .css({
+    textAlign: 'center',
+    color: 'blue'
+  })
+  .appendTo($('header'))
