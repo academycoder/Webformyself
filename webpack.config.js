c4e4b0070839235af578c6e55aa6b7c23ed887fd
fillika <fillika@users.noreply.github.com>
2019-06-02 05:23:24 +0300

    Картинки и копирование
    Настроил копирование в свою папку (настроил его только для картинок) и поддержку url. Проблема в том, что "/" во время dev разработки позволяет видеть картинки, а в продакшен он не пускает. Надо решать

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ const HTMLPLugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // Для сжатия CSS стилей
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin'); // Копирование файлов
 
 
 module.exports = {
   entry: './src/index.js', // Точка входа
   output: {
     filename: 'bundle.js', // Название того файла, который должен получится
-    path: path.resolve(__dirname, 'dist')// Путь, куда мы должны положить все исходные статические файлы, __dirname - это системная переменная, которая указывает ткущую папку, а второй параметр (дист) - куда нужно все это сложить.
+    path: path.resolve(__dirname, 'dist') // Путь, куда мы должны положить все исходные статические файлы, __dirname - это системная переменная, которая указывает ткущую папку, а второй параметр (дист) - куда нужно все это сложить.
   },
   optimization: {
     minimizer: [
@@ -21,7 +22,7 @@ module.exports = {
     contentBase: path.resolve(__dirname, 'dist'), // Указываем публичный путь до нашего финального проекта
     port: 4200
   },
-  plugins:[ 
+  plugins: [
     new HTMLPLugin({ // Плагин, позволяет компилировать файлы HTML с автоматическим добавлением ссылок на js файлы или css
       filename: 'index.html', // Название файла, которое мы получим на выходе в папку dist
       template: './src/pug/index.pug' //Указываем шаблон, который берем за основу
@@ -32,13 +33,24 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({ // Берет мой CSS файл и компилирует его в отдельный файл style css
       filename: 'style.css'
-    })
+    }),
+    new CopyPlugin([{
+        from: 'src/img',
+        to: 'img'
+      } // Создал копирование из папки src/img в папку дист
+    ])
   ],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.css$/, // Указываем расширение файла, на который будет влиять loader
-        use: [MiniCssExtractPlugin.loader,'css-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
+        use: [MiniCssExtractPlugin.loader, 'css-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/, // Указываем расширение файла, на который будет влиять loader
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        }
       },
       {
         test: /\.pug$/, // Указываем расширение файла, на который будет влиять loader
@@ -49,16 +61,16 @@ module.exports = {
       },
       {
         test: /\.less$/, // Указываем расширение файла, на который будет влиять loader
-        use: [MiniCssExtractPlugin.loader,'css-loader', 'less-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
       },
       {
         test: /\.scss$/, // Указываем расширение файла, на который будет влиять loader
-        use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // Обязательно указываем первым style вначале, т.к вебпак считывает справа-налево, т.е. сначала он займется css, а потом style
       },
-      { 
-        test: /\.js$/,       //  Это настройка для babel. Она звучит так: берем файлы с раширение JS, не трогаем те, что в node modules и используем для работы babel-loader
-        exclude: /node_modules/, 
-        loader: "babel-loader" 
+      {
+        test: /\.js$/, //  Это настройка для babel. Она звучит так: берем файлы с раширение JS, не трогаем те, что в node modules и используем для работы babel-loader
+        exclude: /node_modules/,
+        loader: "babel-loader"
       }
     ]
   }
