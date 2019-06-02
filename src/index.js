ed7e7787ec1eb813144145be7b28807ad156c271
fillika <fillika@users.noreply.github.com>
2019-06-02 13:32:09 +0300

    Добавил pug файлы и scss
    Добавил pug файлы, файлы scss для сборки базового шаблона

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import AppService from './modules/app.service'
 import {config} from './modules/config'
 import './modules/header.component'
-// import './modules/images' // Картинки
 import './css/index.css'// Импортирую css файлы
 import './less/index.less'// Импортирую less файлы
 import './scss/index.scss'// Импортирую scss файлы
-// import './image3.png';
 
 console.log('Config key: ', config.key)
 
--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,9 +1,9 @@
 import $ from 'jquery' // Мы не указали полного пути, т.к. вебпак сам понимает, что нужно идти в папку node modules и искать там жуйквери
 
-$('<h1 />')
-  .text('Hello world from JQuery')
-  .css({
-    textAlign: 'center',
-    color: 'blue'
-  })
-  .appendTo($('header'))
+// $('<h1 />')
+//   .text('Hello world from JQuery')
+//   .css({
+//     textAlign: 'center',
+//     color: 'blue'
+//   })
+//   .appendTo($('header'))
